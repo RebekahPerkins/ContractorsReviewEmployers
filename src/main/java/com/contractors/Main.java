d177d7b2710d64d6475eadf1d0b3c780e7285c0a
RebekahPerkins <r.perkins.smith@gmail.com>
2017-05-30 22:56:45 -0700

    handlers in main

--- src/main/java/com/contractors/Main.java
+++ src/main/java/com/contractors/Main.java
@@ -1,10 +1,29 @@
 package com.contractors;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello world");
-
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/sign-in", (req, res) -> {
+            res.cookie("username", req.queryParams("username"));
+            res.redirect("/");
+            return null;
+        });
+        get("/employers", (req, res) -> null);
+        post("/employers", (req, res) -> null);
+        get("/employers/:slug", (req, res) -> null);
+        post("/employers/:slug/vote", (req, res) -> null);
     }
 }
